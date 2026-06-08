31b6882596efd35022de5c7fefa8690d5ac97028
yhsptraa <matthewyohsaputra@gmail.com>
2026-06-08 18:36:37 +0700

    add Prefix Tree insertion and search operations

--- src/com/model/TrieNode.java
+++ src/com/model/TrieNode.java
@@ -1,5 +1,15 @@
 package com.model;
 
+import java.util.HashMap;
+
 public class TrieNode {
-    
+    public HashMap<Character, TrieNode> child;
+    public boolean isEndOfWord;
+    public String songTitle;
+
+    public TrieNode() {
+        this.child = new HashMap<>();
+        this.isEndOfWord = false;
+        this.songTitle = null;
+    }
 }
--- src/com/trie/Trie.java
+++ src/com/trie/Trie.java
@@ -1,5 +1,50 @@
 package com.trie;
 
+import com.model.TrieNode;
+
 public class Trie {
-    
+    private TrieNode root;
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    public void insert(String title) {
+        TrieNode curr = root;
+        String lowerTitle = title.toLowerCase();
+
+        for (int i = 0; i < lowerTitle.length(); i++) {
+            char c = lowerTitle.charAt(i);
+
+            if (!curr.child.containsKey(c)) {
+                curr.child.put(c, new TrieNode());
+            }
+
+            curr = curr.child.get(c);
+        }
+
+        curr.isEndOfWord = true;
+        curr.songTitle = title;
+    }
+
+    public TrieNode searchPrefix(String prefix) {
+        TrieNode curr = root;
+        String lowerPrefix = prefix.toLowerCase();
+
+        for (int i = 0; i < lowerPrefix.length(); i++) {
+            char c = lowerPrefix.charAt(i);
+
+            if (!curr.child.containsKey(c)) {
+                return null;
+            }
+
+            curr = curr.child.get(c);
+        }
+
+        return curr;
+    }
+
+    public TrieNode getRoot() {
+        return root;
+    }
 }
